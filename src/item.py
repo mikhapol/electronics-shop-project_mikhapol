d09949ecaa5ad5021aec32817fe3bb719c43feab
Mikhail Polyakov PyCharm <mikhapol@bk.ru>
2023-04-29 18:18:42 +0300

    Add homework-3

--- src/item.py
+++ src/item.py
@@ -1,4 +1,8 @@
+import os  # Подключение модуля OS - адаптация пути к файлам в ОС.
 import csv
+
+BASE_PATH = os.path.abspath("../src")  # Применение абсолютного пути.
+FILE_ITEMS = os.path.join(BASE_PATH, "items.csv")
 
 
 class Item:
@@ -12,7 +16,6 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
@@ -24,8 +27,14 @@ class Item:
         Item.all.append(self)
         Item.all_name.append(name)
 
+    def __repr__(self) -> str:
+        return f'{__class__.__name__}({self.__name!r}, {self.price}, {self.quantity})'
+
+    def __str__(self) -> str:
+        return str(self.name)
+
     @property
-    def name(self):
+    def name(self) -> str:
         """
         Геттер приватного параметра name
         :return:
@@ -33,7 +42,7 @@ class Item:
         return self.__name
 
     @name.setter
-    def name(self, name: str):
+    def name(self, name: str) -> None:
         """
         Сеттер приватного параметра который проверяет, что длина наименования товара не больше 10 символов
         """
@@ -43,7 +52,6 @@ class Item:
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
@@ -55,12 +63,12 @@ class Item:
         self.price = self.price * self.pay_rate
 
     @staticmethod
-    def string_to_number(number):
+    def string_to_number(number: float) -> int:
         return int(float(number))
 
     @classmethod
-    def instantiate_from_csv(cls):
-        with open("src/items.csv", "r", encoding='windows-1251') as csv_file:
+    def instantiate_from_csv(cls) -> None:
+        with open(FILE_ITEMS, "r", encoding='windows-1251') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
                 cls(row["name"], row["price"], row["quantity"])
